96f498ee58fd1cda279bf145d489c29b28139b33
Chris Grandin <chris.grandin@gmail.com>
2025-03-27 03:59:03 -0700

    Add comment to create vignettes script

--- vignettes/create-vignettes.R
+++ vignettes/create-vignettes.R
@@ -1,7 +1,7 @@
 # Source this script to build and install all csasdown vignettes on your machine
 
 root_dir <- here::here()
-inst_doc_dir <- here::here("inst/doc")
+inst_doc_dir <- file.path(root_dir, "inst/doc")
 
 build_vignettes(root_dir)
 dir.create(inst_doc_dir, showWarnings = FALSE)
@@ -10,4 +10,6 @@ file.copy(dir("doc", full.names = TRUE),
           overwrite = TRUE)
 devtools::install(root_dir)
 
+# You may have to run this manually after the script completes. For
+# an unknown reason it doesn't launch the HTML page on the browser
 utils::browseVignettes("csasdown")
